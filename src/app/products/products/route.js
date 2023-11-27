e1bd12b8e8f4848a216f616c41bb3936e6026e91
ftorcas3110 <borjatorres1228@gmail.com>
2023-11-27 18:48:17 +0100

    update

--- src/app/products/products/route.js
+++ src/app/products/products/route.js
@@ -18,8 +18,8 @@ export async function POST(request) {
     const { database } = await connectToDatabase();
     const collection = database.collection(process.env.MONGODB_COLLECTION)
 
-    const { nombre, edad } = await request.json() // Read body request
-    const results = await collection.insertOne({ nombre, edad });
+    const { nombre, stock } = await request.json() // Read body request
+    const results = await collection.insertOne({ nombre, stock });
 
     return Response.json(results)
 }
